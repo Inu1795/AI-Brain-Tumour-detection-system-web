c65893f675e115c5adc77b9fad9224e2690ecd67
Inu1795 <ishan.sr07@gmail.com>
2022-03-24 19:15:18 +0530

    flask file updated

--- market.py
+++ market.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')  # this is a route, a term of Flask called as decorator
 @app.route('/home')
-def app_mp():
+def app_iji_loginpage():
     # render template is an inbuilt function which renders file from app_mp html file
     return render_template('loginpage.html')
 
@@ -13,3 +13,4 @@ def app_mp():
 @app.route('/login')
 def login_page():
     return render_template('loginpage.html')
+
